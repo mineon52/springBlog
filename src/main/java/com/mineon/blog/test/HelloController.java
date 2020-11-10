536f9d61daaf1aa0dadda370faaae39a3d2f5885
ZAIT <ZAIT@172.16.14.17>
2020-11-10 13:48:08 +0900

    1

---
+++ src/main/java/com/mineon/blog/test/HelloController.java
@@ -0,0 +1,14 @@
+package com.mineon.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "<h1>Hello Spring boot</h1> ";
+	}
+}
